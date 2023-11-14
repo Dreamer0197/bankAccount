5b754e79e0c56e2b2fab2312c54f0b76ca7b3c03
Kaan Şengün <427642@ogr.ktu.edu.tr>
2023-11-14 21:53:25 +0300

    Initial Commit

---
+++ bankAccountt/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  bankAccountt
+//
+//  Created by Kaan Şengün on 14.11.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
